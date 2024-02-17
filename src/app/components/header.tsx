1586905029e8aeb257bf7b0b70695ca6468931c5
JoaoFPessoa <joaoppessoa@uol.com.br>
2024-02-16 21:59:07 -0300

    add: Header

---
+++ src/app/components/header.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+import Logo from '../../../public/logo-verde-branco.png';
+import Navbar from './navbar';
+export default function Header() {
+	return (
+		<div className=" h-[100px] sticky border-b border-gray-200 bg-grayBackground top-0 left-0 w-full z-50">
+			<header className="flex justify-between px-5 py-5">
+				<div>
+					<Image width={200} height={200} alt="logo-paktor" src={Logo} />
+				</div>
+				<div className="h-[2000px] w-[40%] ">
+					<Navbar />
+				</div>
+			</header>
+		</div>
+	);
+}
