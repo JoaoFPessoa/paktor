cc364f8c4781e668f7c9a5d3991f0779bdfc6903
JoaoFPessoa <joaoppessoa@uol.com.br>
2024-02-17 12:02:33 -0300

    add: Testimonials cards

---
+++ src/app/components/Sections/Testimonials/TestimonialCard.tsx
@@ -0,0 +1,39 @@
+import { FaUserCircle } from 'react-icons/fa';
+import Star from '../../../../../public/starts.png';
+import Image from 'next/image';
+
+type Props = {
+	name: string;
+	description: string;
+	stars: number;
+	date: string;
+};
+
+export default function TestimonialCard({
+	name,
+	description,
+	date,
+	stars,
+}: Props) {
+	const starElements = Array.from({ length: stars }, (_, index) => (
+		<Image key={index} src={Star} alt="star" />
+	));
+
+	return (
+		<div className="">
+			<div id="profile" className="m-1 flex items-start">
+				<FaUserCircle className=" w-[40px] h-[40px]" />
+				<div className="ml-2">
+					<h4 className="text-lg font-medium leading-6">{name}</h4>
+					<span className="text-sm text-gray-500">{date}</span>
+				</div>
+			</div>
+			<div id="rating" className="m-1 mt-3 flex">
+				{starElements}
+			</div>
+			<div id="description" className="mt-3">
+				{description}
+			</div>
+		</div>
+	);
+}
