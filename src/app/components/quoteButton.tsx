c3e8342cbb50aeb7585b6b28543a7c9120311589
JoaoFPessoa <joaoppessoa@uol.com.br>
2024-02-16 21:58:43 -0300

    add: QuoteButton

---
+++ src/app/components/quoteButton.tsx
@@ -0,0 +1,7 @@
+export default function QuoteButton() {
+	return (
+		<button className="bg-buttonGreen rounded-xl font-bold p-4 transform hover:scale-105 transition duration-300">
+			Faça uma cotação
+		</button>
+	);
+}
