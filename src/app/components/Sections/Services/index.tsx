19eb0b38f573000d2356bcb91249f0c7a9a58ae6
JoaoFPessoa <joaoppessoa@uol.com.br>
2024-02-17 12:37:55 -0300

    add Services Section

---
+++ src/app/components/Sections/Services/index.tsx
@@ -0,0 +1,19 @@
+export default function ServicesSection() {
+	return (
+		<div className="pt-10">
+			<h1 className="leading-45 font-extrabold tracking-tight text-4xl">
+				Nossos serviços
+			</h1>
+			<h2 className=" text-gray-800 w-[60%] py-3">
+				Executando reformas em <b>casas, apartamentos e salas comerciais</b>,
+				bem como construindo empreendimentos desde a fundação até o acabamento
+				final, possuindo um forte viés sustentável -{' '}
+				<b>energia solar e água de reúso</b> - como principal diferencial em
+				seus projetos, agregando valores da vivência em todas as etapas da obra,
+				com o conhecimento técnico necessário para a elaboração dos mesmos, para
+				oferecer ao cliente a
+				<b> melhor experiência possível, sem dor de cabeça.</b>
+			</h2>
+		</div>
+	);
+}
