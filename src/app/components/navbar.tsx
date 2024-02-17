7c3d732f8f2059139ce3052a288ff9959c70df7f
JoaoFPessoa <joaoppessoa@uol.com.br>
2024-02-16 21:58:55 -0300

    add: Navbar

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,14 @@
+import Button from './button';
+import QuoteButton from './quoteButton';
+
+export default function Navbar() {
+	return (
+		<div className="flex gap-3 justify-between px-5 items-center font-semibold">
+			<Button>Sobre</Button>
+			<Button>Serviços</Button>
+			<Button>Projetos</Button>
+			<Button>Contatos</Button>
+			<QuoteButton />
+		</div>
+	);
+}
