b1c6f2942b42628b3938ed5db90d480311dbb915
JoaoFPessoa <joaoppessoa@uol.com.br>
2024-02-17 12:01:18 -0300

    chore: removed unused lines

--- src/app/carouselClientPage.tsx
+++ src/app/carouselClientPage.tsx
@@ -1,8 +1,4 @@
 'use client';
-import ImageOne from '../../public/services-image-1.png';
-import ImageTwo from '../../public/services-image-2.png';
-import ImageThree from '../../public/services-image-3.png';
-
 import ImageCarousel from './components/Carousel';
 
 export default function CarouselClientPage() {
