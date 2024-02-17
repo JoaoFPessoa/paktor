c0d77cdb45be10269466efcf6a00ee7dd2e1ea51
JoaoFPessoa <joaoppessoa@uol.com.br>
2024-02-17 12:02:49 -0300

    add: Projectscomponent

---
+++ src/app/components/Sections/Projects/index.tsx
@@ -0,0 +1,9 @@
+export default function Projects() {
+	return (
+		<div className="pt-10">
+			<h1 className="leading-45 font-extrabold tracking-tight text-4xl">
+				Nossos projetos
+			</h1>
+		</div>
+	);
+}
