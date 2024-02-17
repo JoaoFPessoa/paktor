52e7249629b5a70bcd5dd32f73ffb84a546c372a
JoaoFPessoa <joaoppessoa@uol.com.br>
2024-02-17 11:19:10 -0300

    fix: wrong style

--- src/app/components/Sections/Header/header.tsx
+++ src/app/components/Sections/Header/header.tsx
@@ -4,11 +4,11 @@ import Navbar from './navbar';
 export default function Header() {
 	return (
 		<div className=" h-[100px] sticky border-b border-gray-200 bg-grayBackground top-0 left-0 w-full z-50">
-			<header className="flex justify-between px-5 py-5">
+			<header className="flex justify-between px-10 py-5">
 				<div>
 					<Image width={200} height={200} alt="logo-paktor" src={Logo} />
 				</div>
-				<div className="h-[2000px] w-[40%] ">
+				<div className=" w-[40%] ">
 					<Navbar />
 				</div>
 			</header>
