e0e0e5f4dabd57b9918c5ff1b38a601f0e6f7ea8
JoaoFPessoa <joaoppessoa@uol.com.br>
2024-02-28 12:16:03 -0300

    responsivity

--- src/app/components/Sections/Contact/index.tsx
+++ src/app/components/Sections/Contact/index.tsx
@@ -4,12 +4,14 @@ export default function Contact() {
 	return (
 		<div
 			id="contact"
-			className="flex flex-col items-center justify-center my-16"
+			className="flex flex-col items-center justify-center my-5 md:my-16"
 		>
-			<h1 className="text-4xl font-bold tracking-tighter">
+			<h1 className="text-3xl md:text-4xl font-bold tracking-tighter">
 				Pronto para começar seu projeto?
 			</h1>
-			<p className="mb-8 mt-2">Contate-nos hoje para uma consulta gratuita</p>
+			<p className="w-full md:w-auto mb-8 mt-2">
+				Contate-nos hoje para uma consulta gratuita
+			</p>
 			<QuoteButton />
 		</div>
 	);
