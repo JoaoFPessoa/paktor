37c4a04fc7d6151567b0e2a0b163daef6dfeb26a
JoaoFPessoa <joaoppessoa@uol.com.br>
2024-02-28 11:52:24 -0300

    responsivity: header

--- src/app/components/Sections/Header/header.tsx
+++ src/app/components/Sections/Header/header.tsx
@@ -1,14 +1,28 @@
 import Image from 'next/image';
 import Logo from '../../../../../public/logo-verde-branco.png';
+import MobileLogo from '../../../../../public/mobile-logo.png';
 import Navbar from './navbar';
 export default function Header() {
 	return (
 		<div className=" h-[100px] sticky border-b border-gray-200 bg-grayBackground top-0 left-0 w-full z-50">
 			<header className="flex justify-between px-10 py-5">
 				<div>
-					<Image width={200} height={200} alt="logo-paktor" src={Logo} />
+					<Image
+						width={80}
+						height={80}
+						className="md:hidden"
+						alt="logo-paktor"
+						src={MobileLogo}
+					/>
+					<Image
+						width={200}
+						height={200}
+						className="hidden"
+						alt="logo-paktor"
+						src={Logo}
+					/>
 				</div>
-				<div className=" w-[40%] ">
+				<div className="w-[80%]  md:w-[40%] ">
 					<Navbar />
 				</div>
 			</header>
